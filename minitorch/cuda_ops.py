2ea28d57247f0293c603da0a17deab9317ed4886
Pavan Narahari <naraharip@wustl.edu>
2024-11-21 04:03:09 -0500

    fix

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -481,19 +481,14 @@ def _tensor_matrix_multiply(
     acc = 0.0
     for k in range(0,num_cols_a_rows_b, BLOCK_DIM):
         if i < num_rows_a and pj + k < num_cols_a_rows_b:
-            a_shared[pi, pj] = a_storage[(min(batch, a_shape[0] - 1)) * a_batch_stride + i * a_strides[-2] + pj + k]
+            a_shared[pi, pj] = a_storage[batch * a_batch_stride + i * a_strides[-2] + pj + k]
             # a_shared[pi, pj] = a_storage[batch * a_batch_stride + pi * a_strides[-2] + pj + k]
-        else:
-            a_shared[pi, pj] = 0.0
         if pi + k < num_cols_a_rows_b and j < num_cols_b:
-            b_shared[pi, pj] = b_storage[(min(batch, b_shape[0] - 1)) * b_batch_stride + (pi+k) * b_strides[-2] + j]
+            b_shared[pi, pj] = b_storage[batch * b_batch_stride + (pi+k) * b_strides[-2] + j]
             # b_shared[pi, pj] = b_storage[batch * b_batch_stride + pi * b_strides[-2] + pj + k]
-        else:
-            b_shared[pi, pj] = 0.0
         cuda.syncthreads()
         for local_k in range(min(BLOCK_DIM, num_cols_a_rows_b - k)):
             acc += a_shared[pi, local_k] * b_shared[local_k, pj]
-            
     if batch < out_shape[0] and i < out_shape[1] and j < out_shape[2]:
         out_pos = batch * out_strides[0] + i * out_strides[1] + j
         out[out_pos] = acc
