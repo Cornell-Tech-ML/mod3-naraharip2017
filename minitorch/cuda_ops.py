b4ce4de768fe9c4909f7c765db012d41a3c74402
Pavan Narahari <naraharip@wustl.edu>
2024-11-21 15:49:10 -0500

    adding end strides

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -482,9 +482,9 @@ def _tensor_matrix_multiply(
     acc = 0.0
     for k in range(0,num_cols_a_rows_b, BLOCK_DIM):
         if i < num_rows_a and (pj + k) < num_cols_a_rows_b:
-            a_shared[pi, pj] = a_storage[batch * a_batch_stride + i * a_strides[-2] + pj + k]
+            a_shared[pi, pj] = a_storage[batch * a_batch_stride + i * a_strides[-2] + (pj + k) * a_strides[-1]]
         if pi + k < num_cols_a_rows_b and j < num_cols_b:
-            b_shared[pi, pj] = b_storage[batch * b_batch_stride + (pi+k) * b_strides[-2] + j]
+            b_shared[pi, pj] = b_storage[batch * b_batch_stride + (pi+k) * b_strides[-2] + j * b_strides[-1]]
         cuda.syncthreads()
         for local_k in range(min(BLOCK_DIM, num_cols_a_rows_b - k)):
             acc += a_shared[pi, local_k] * b_shared[local_k, pj]
